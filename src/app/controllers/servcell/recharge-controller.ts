688a95a3324a5b8ad4dfc7ac81866d37d859710f
Lucas Santos <lucas.santos@pagtel.com.br>
2020-12-11 19:56:12 +0000

    feat: add answer 01 for refills in progress

--- src/app/controllers/servcell/recharge-controller.ts
+++ src/app/controllers/servcell/recharge-controller.ts
@@ -15,17 +15,23 @@ import { buildXml } from '../../helpers/xml'
 import { rechargeYupSchema } from '../../validation/yup'
 
 interface NuageRequestsValues {
-  rechargeValue: string
-  creditValue: string
+  rechargeValue: string;
+  creditValue: string;
 }
 
 const INTER_BANK_ORIGIN = '6302'
 
-const isInterRecharge40WithDiscount = (value: string, origin: string) => (value === '10800' || value === '10200') && origin === INTER_BANK_ORIGIN
-const isInterRecharge50WithDiscount = (value: string, origin: string) => (value === '13500' || value === '12750') && origin === INTER_BANK_ORIGIN
-const isInterRecharge75WithDiscount = (value: string, origin: string) => (value === '20250' || value === '19125') && origin === INTER_BANK_ORIGIN
+const isInterRecharge40WithDiscount = (value: string, origin: string) =>
+  (value === '10800' || value === '10200') && origin === INTER_BANK_ORIGIN
+const isInterRecharge50WithDiscount = (value: string, origin: string) =>
+  (value === '13500' || value === '12750') && origin === INTER_BANK_ORIGIN
+const isInterRecharge75WithDiscount = (value: string, origin: string) =>
+  (value === '20250' || value === '19125') && origin === INTER_BANK_ORIGIN
 
-const nuageRequests = (body: any, responseTopUp: any) => async ({ rechargeValue, creditValue }: NuageRequestsValues) => {
+const nuageRequests = (body: any, responseTopUp: any) => async ({
+  rechargeValue,
+  creditValue
+}: NuageRequestsValues) => {
   const dtExecucao = dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss')
 
   const responseNuage = await NuageModel.procRecargaNuage({
@@ -36,7 +42,11 @@ const nuageRequests = (body: any, responseTopUp: any) => async ({ rechargeValue,
     nsu: responseTopUp.transactionID
   })
 
-  saveControllerLogs('POS RECARGA NUAGE ', { body, response: responseNuage }, 'servcelRecarga-controller')
+  saveControllerLogs(
+    'POS RECARGA NUAGE ',
+    { body, response: responseNuage },
+    'servcelRecarga-controller'
+  )
 
   const responseCredit = await NuageModel.addCredit({
     msisdn: '55' + body.msisdn,
@@ -46,11 +56,19 @@ const nuageRequests = (body: any, responseTopUp: any) => async ({ rechargeValue,
     nsu: responseTopUp.transactionID
   })
 
-  saveControllerLogs('POS ADD CREDIT NUAGE ', { body, response: responseCredit }, 'servcelRecarga-controller')
+  saveControllerLogs(
+    'POS ADD CREDIT NUAGE ',
+    { body, response: responseCredit },
+    'servcelRecarga-controller'
+  )
 }
 
 export const RechargeController = (req: Request, res: Response) => {
-  saveControllerLogs('INICIO            ', req.body, 'servcelRecarga-controller')
+  saveControllerLogs(
+    'INICIO            ',
+    req.body,
+    'servcelRecarga-controller'
+  )
 
   let statusCode: number = 200
   const response: IServCelResponse = {
@@ -61,31 +79,74 @@ export const RechargeController = (req: Request, res: Response) => {
     rechargeYupSchema
       .validate(req.body.xml)
       .then(async (body: any) => {
-        saveControllerLogs('POS VALID PARAMS  ', body, 'servcelRecarga-controller')
+        saveControllerLogs(
+          'POS VALID PARAMS  ',
+          body,
+          'servcelRecarga-controller'
+        )
 
-        const servCelResponse: IServCelInsResponse = await ServCelModel.procInsServCel('Recarga', 200, '', null, body)
+        const servCelResponse: IServCelInsResponse = await ServCelModel.procInsServCel(
+          'Recarga',
+          200,
+          '',
+          null,
+          body
+        )
 
-        saveControllerLogs('POS PROC 200      ', { body, response: servCelResponse }, 'servcelRecarga-controller')
+        saveControllerLogs(
+          'POS PROC 200      ',
+          { body, response: servCelResponse },
+          'servcelRecarga-controller'
+        )
 
         const checkPlintron = await ServCelModel.procCheckPlintron()
 
-        saveControllerLogs('POS SELECT CHECK  ', { body, response: checkPlintron }, 'servcelRecarga-controller')
+        saveControllerLogs(
+          'POS SELECT CHECK  ',
+          { body, response: checkPlintron },
+          'servcelRecarga-controller'
+        )
+
+        // Perdoai qualquer coisa, código ruim se resolve com mais código ruim.
+        // Quebra o fluxo atual e responde para os caras que jé processamos a solicitação.
+        if (servCelResponse.code === '01') {
+          return res.format({
+            'application/xml': () => {
+              res.status(statusCode).send(buildXml(servCelResponse.code))
+            }
+          })
+        }
 
         if (body.msisdn.length === 11) {
-          saveControllerLogs('POS VALID MSISDN  ', body, 'servcelRecarga-controller')
+          saveControllerLogs(
+            'POS VALID MSISDN  ',
+            body,
+            'servcelRecarga-controller'
+          )
 
           if (await NuageModel.procContaNuage(body.msisdn)) {
-            saveControllerLogs('POS CONTA NUAGE   ', body, 'servcelRecarga-controller')
+            saveControllerLogs(
+              'POS CONTA NUAGE   ',
+              body,
+              'servcelRecarga-controller'
+            )
 
             if (checkPlintron) {
-              const responseGetAuth: IGetAuthResponse = await ServCelModel.procGetAuth(body.msisdn, body.operadora)
-              saveControllerLogs('POS PROC GETAUTH  ', { body, response: responseGetAuth }, 'servcelRecarga-controller')
+              const responseGetAuth: IGetAuthResponse = await ServCelModel.procGetAuth(
+                body.msisdn,
+                body.operadora
+              )
+              saveControllerLogs(
+                'POS PROC GETAUTH  ',
+                { body, response: responseGetAuth },
+                'servcelRecarga-controller'
+              )
 
               const dateNow = new Date()
               const transactionID: string = (
                 'SC' +
-                  servCelResponse.idServCel +
-                  dateFormat(dateNow, 'yyyymmdhhMMss')
+                servCelResponse.idServCel +
+                dateFormat(dateNow, 'yyyymmdhhMMss')
               ).padStart(19, '0')
 
               const requestTopUp: ITopUpRequest = {
@@ -105,7 +166,11 @@ export const RechargeController = (req: Request, res: Response) => {
                 requestTopUp
               )
 
-              saveControllerLogs('POSPROCINSPLINTRON', { body, response: responseTopUp }, 'servcelRecarga-controller')
+              saveControllerLogs(
+                'POSPROCINSPLINTRON',
+                { body, response: responseTopUp },
+                'servcelRecarga-controller'
+              )
 
               if (responseTopUp.code === '00') {
                 response.codResposta = '00'
@@ -115,12 +180,36 @@ export const RechargeController = (req: Request, res: Response) => {
 
                 const requests = nuageRequests(body, responseTopUp)
 
-                if (isInterRecharge40WithDiscount(stringRechargeValue, rechargeOrigin)) {
-                  await requests({ rechargeValue: '4000', creditValue: '8000' })
-                } else if (isInterRecharge50WithDiscount(stringRechargeValue, rechargeOrigin)) {
-                  await requests({ rechargeValue: '5000', creditValue: '10000' })
-                } else if (isInterRecharge75WithDiscount(stringRechargeValue, rechargeOrigin)) {
-                  await requests({ rechargeValue: '7500', creditValue: '15000' })
+                if (
+                  isInterRecharge40WithDiscount(
+                    stringRechargeValue,
+                    rechargeOrigin
+                  )
+                ) {
+                  await requests({
+                    rechargeValue: '4000',
+                    creditValue: '8000'
+                  })
+                } else if (
+                  isInterRecharge50WithDiscount(
+                    stringRechargeValue,
+                    rechargeOrigin
+                  )
+                ) {
+                  await requests({
+                    rechargeValue: '5000',
+                    creditValue: '10000'
+                  })
+                } else if (
+                  isInterRecharge75WithDiscount(
+                    stringRechargeValue,
+                    rechargeOrigin
+                  )
+                ) {
+                  await requests({
+                    rechargeValue: '7500',
+                    creditValue: '15000'
+                  })
                 } else {
                   // Recarga Nuage ********************************************
                   const requestRecarga: IRecargaRequest = {
@@ -131,9 +220,15 @@ export const RechargeController = (req: Request, res: Response) => {
                     nsu: responseTopUp.transactionID
                   }
 
-                  const responseNuage = await NuageModel.procRecargaNuage(requestRecarga)
+                  const responseNuage = await NuageModel.procRecargaNuage(
+                    requestRecarga
+                  )
 
-                  saveControllerLogs('POS RECARGA NUAGE ', { body, response: responseNuage }, 'servcelRecarga-controller')
+                  saveControllerLogs(
+                    'POS RECARGA NUAGE ',
+                    { body, response: responseNuage },
+                    'servcelRecarga-controller'
+                  )
                 }
               } else {
                 response.codResposta = '10'
@@ -142,9 +237,16 @@ export const RechargeController = (req: Request, res: Response) => {
               if (servCelResponse.code === '01') {
                 response.codResposta = servCelResponse.code
               } else {
-                const responseApi: IServCelResponse = await ServCelModel.procGetCodResposta(body.msisdn, 'Recarga')
+                const responseApi: IServCelResponse = await ServCelModel.procGetCodResposta(
+                  body.msisdn,
+                  'Recarga'
+                )
 
-                saveControllerLogs('POSPROCCODRESPOSTA', { body, response: responseApi }, 'servcelRecarga-controller')
+                saveControllerLogs(
+                  'POSPROCCODRESPOSTA',
+                  { body, response: responseApi },
+                  'servcelRecarga-controller'
+                )
 
                 if (responseApi) {
                   response.codResposta = responseApi.codResposta
@@ -166,8 +268,16 @@ export const RechargeController = (req: Request, res: Response) => {
           body
         )
 
-        saveControllerLogs('POS PROC 210      ', { body, response: responsePro210 }, 'servcelRecarga-controller')
-        saveControllerLogs('FIM               ', body, 'servcelRecarga-controller')
+        saveControllerLogs(
+          'POS PROC 210      ',
+          { body, response: responsePro210 },
+          'servcelRecarga-controller'
+        )
+        saveControllerLogs(
+          'FIM               ',
+          body,
+          'servcelRecarga-controller'
+        )
 
         return res.format({
           'application/xml': () => {
@@ -176,7 +286,11 @@ export const RechargeController = (req: Request, res: Response) => {
         })
       })
       .catch((err: any) => {
-        saveControllerLogs('ERROR            ', { body: req.body, error: err.toString() }, 'servcelRecarga-controller')
+        saveControllerLogs(
+          'ERROR            ',
+          { body: req.body, error: err.toString() },
+          'servcelRecarga-controller'
+        )
 
         statusCode = 400
         console.log(err)
@@ -188,7 +302,11 @@ export const RechargeController = (req: Request, res: Response) => {
         })
       })
   } catch (err) {
-    saveControllerLogs('ERROR            ', { body: req.body, error: err.toString() }, 'servcelRecarga-controller')
+    saveControllerLogs(
+      'ERROR            ',
+      { body: req.body, error: err.toString() },
+      'servcelRecarga-controller'
+    )
 
     statusCode = 400
     console.log(err)
